d2fec35ec0c6ddd239f89f7ef93869059fe469c6
Oliver Ni <oliverni@berkeley.edu>
2025-06-05 21:39:24 -0700

    refactor: simplify unordered pairs

---
+++ src/editor/board/util.rs
@@ -0,0 +1,27 @@
+use crate::bpz::Pos;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct PosOrd {
+    pub row: i32,
+    pub col: i32,
+}
+
+impl From<Pos> for PosOrd {
+    fn from(Pos { row, col }: Pos) -> Self {
+        Self { row, col }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct UnorderedPair<T: Ord>(T, T);
+
+impl<T: Ord> UnorderedPair<T> {
+    pub fn new(a: impl Into<T>, b: impl Into<T>) -> Self {
+        let mut a = a.into();
+        let mut b = b.into();
+        if b < a {
+            std::mem::swap(&mut a, &mut b);
+        }
+        Self(a, b)
+    }
+}
